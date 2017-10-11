c4223e7a3065fe75b6e6edf4e4e590f7621bb492
sonyahon <volkov030@gmail.com>
2017-10-11 21:26:51 +0300

    fixed malloc error

--- printf/main.c
+++ printf/main.c
@@ -7,8 +7,10 @@
 
 int main(int argc, char ** argv) {
     
-    int f = open("res_file", O_CREAT | O_RDWR);
+    int f = open("res_file", O_CREAT | O_RDWR, 0666);
     print_myf(f, "%s", "Hello world\n");
+    close(f);
+
     print_my("This is string - %s, this is decimal %d, this is float %f\n", "Hello World", 12, 120.001);
 
     return 0;
